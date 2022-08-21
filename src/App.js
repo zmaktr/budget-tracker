80f7d8fb68d2fca8383d040a6078770c2e26b438
Zaeem Akhtar <zmaktr@gmail.com>
2022-08-21 19:53:56 +0500

    UI elements in place

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import Budget from './components/Budget';
 import Remaining from './components/Remaining'
 import ExpenseTotal from './components/ExpenseTotal'
 import ExpenseList from './components/ExpenseList'
+import AddExpenseForm from './components/AddExpenseForm'
 
 const App = () => {
   return (
@@ -22,10 +23,16 @@ const App = () => {
         </div>
       </div>
 
-      <h3 className='mt3'>Expenses</h3>
+      <h3 className='mt-3'>Expenses</h3>
       <div className='row mt-3'>
         <div className='col-sm'>
           <ExpenseList/>
+        </div>
+      </div>
+      <h3 className='mt-3'>Add Expense</h3>
+      <div className='mt-3'>
+        <div className='col-sm'>
+          <AddExpenseForm/>
         </div>
       </div>
     </div>
